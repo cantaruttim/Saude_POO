0d6e09dd5158a61462583a42610e4726ffec61ad
cantaruttim@outlook.com <81988636+cantaruttim@users.noreply.github.com>
2023-01-12 17:26:51 -0300

    DocString

--- paciente.py
+++ paciente.py
@@ -3,6 +3,15 @@ from medicamento import Medicamento
 
 class Pessoa:
 
+    """
+    Representa a entidade Pessoa/Paciente.
+
+    nome (str) : nome do paciente
+    idade (int) : idade do paciente
+
+
+    """
+
     # a ideia é fazer com que a IA indique a possível causa (diagnóstico) e um tratamento
     def __init__(self, nome, idade, queixa, medicamentos):
         self.nome = nome
